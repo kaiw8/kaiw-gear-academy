f5474f602ee2e3beedb64a370a7dca2df5b99853
kaiw8 <qwqa8887@gmail.com>
2024-08-05 23:02:38 +0800

    Revert "fin"

--- wordle-game/wordle-game-session/build.rs
+++ wordle-game/wordle-game-session/build.rs
@@ -1,4 +1,4 @@
-use game_session_io::WordleSessionMetadata as Metadata;
+use wordle_game_session_io::WordleSessionMetadata as Metadata;
 
 fn main() {
     gear_wasm_builder::build_with_metadata::<Metadata>();
