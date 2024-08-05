2ab6f10bf1b2ba7e666b65eaa8af4ab0dad29233
kaiw8 <qwqa8887@gmail.com>
2024-08-05 10:20:30 +0800

    Merge pull request

--- wordle-game/wordle-game-session/tests/tests.rs
+++ wordle-game/wordle-game-session/tests/tests.rs
@@ -10,7 +10,7 @@ fn get_program(system: &System) -> Program {
     let session = Program::current(system);
     let wordle = Program::from_file(
         system,
-        "../target/wasm32-unknown-unknown/release/wordle_game.opt.wasm",
+        "../target/wasm32-unknown-unknown/debug/wordle_game.opt.wasm",
     );
 
     assert_eq!(session.id(), 1.into());
